f16bb33ce538be769f5282e832c4f72257c2c65b
Katarina Sheremet <kate.romanowskaya@gmail.com>
2015-07-30 23:39:00 +0200

    Initial commit

---
+++ crawler.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+        "fmt"
+	"golang.org/x/net/html"
+	"net/http"
+        "flag"
+        "strings"
+
+
+)
+
+func main() {
+
+     var adr = flag.String("flag", "http://xmpp.org", "http address")
+     flag.Parse()
+
+     //make reguest
+     resp, _ := http.Get(*adr)
+
+     //return new html tokenizer for the given Reader
+     tz := html.NewTokenizer(resp.Body)
+     for {
+         // scan the next token and return its type
+         token := tz.Next()
+
+         switch token {
+         case  html.ErrorToken:
+               // End of the document
+	       return
+	 case html.StartTagToken, html.SelfClosingTagToken:
+              tag, hattr :=tz.TagName()
+	      isAnchor := string(tag)
+
+	      if isAnchor == "a" && hattr {
+              _, v, _ :=tz.TagAttr()
+              res :=string(v)
+              if strings.HasPrefix(res,"http://") {
+                 fmt.Println(res)
+                 } else { fmt.Println(*adr+res)
+                        }
+                 }
+              }
+     }
+     resp.Body.Close()
+}
