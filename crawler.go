959efeaa2aaaa8826406697eaf3a05fd6abc1d23
Katarina Sheremet <kate.romanowskaya@gmail.com>
2015-07-30 23:58:42 +0200

    Formatting fix

--- crawler.go
+++ crawler.go
@@ -1,46 +1,45 @@
 package main
 
 import (
-        "fmt"
+	"flag"
+	"fmt"
 	"golang.org/x/net/html"
 	"net/http"
-        "flag"
-        "strings"
-
-
+	"strings"
 )
 
 func main() {
 
-     var adr = flag.String("flag", "http://xmpp.org", "http address")
-     flag.Parse()
+	var adr = flag.String("flag", "http://xmpp.org", "http address")
+	flag.Parse()
 
-     //make reguest
-     resp, _ := http.Get(*adr)
+	//make reguest
+	resp, _ := http.Get(*adr)
 
-     //return new html tokenizer for the given Reader
-     tz := html.NewTokenizer(resp.Body)
-     for {
-         // scan the next token and return its type
-         token := tz.Next()
+	//return new html tokenizer for the given Reader
+	tz := html.NewTokenizer(resp.Body)
+	for {
+		// scan the next token and return its type
+		token := tz.Next()
 
-         switch token {
-         case  html.ErrorToken:
-               // End of the document
-	       return
-	 case html.StartTagToken, html.SelfClosingTagToken:
-              tag, hattr :=tz.TagName()
-	      isAnchor := string(tag)
+		switch token {
+		case html.ErrorToken:
+			// End of the document
+			return
+		case html.StartTagToken, html.SelfClosingTagToken:
+			tag, hattr := tz.TagName()
+			isAnchor := string(tag)
 
-	      if isAnchor == "a" && hattr {
-              _, v, _ :=tz.TagAttr()
-              res :=string(v)
-              if strings.HasPrefix(res,"http://") {
-                 fmt.Println(res)
-                 } else { fmt.Println(*adr+res)
-                        }
-                 }
-              }
-     }
-     resp.Body.Close()
+			if isAnchor == "a" && hattr {
+				_, v, _ := tz.TagAttr()
+				res := string(v)
+				if strings.HasPrefix(res, "http://") {
+					fmt.Println(res)
+				} else {
+					fmt.Println(*adr + res)
+				}
+			}
+		}
+	}
+	resp.Body.Close()
 }
