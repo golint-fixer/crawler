213b128213b4d7e4b9026c05dde1694bd52f4102
Katarina Sheremet <kate.romanowskaya@gmail.com>
2015-08-03 21:15:48 +0200

    Fix presentation of links

--- crawler.go
+++ crawler.go
@@ -8,13 +8,24 @@ import (
 	"strings"
 )
 
-func main() {
+func linkMaker(root, curr *string, l string) string {
+	if strings.HasPrefix(l, "http://") ||
+		(strings.HasPrefix(l, "https://")) {
+		return l
+	}
+	if strings.HasPrefix(l, "/") {
+		return (*root + l)
+	} else {
+		return (*curr + "/" + l)
+	}
 
-	var adr = flag.String("flag", "http://xmpp.org", "http address")
-	flag.Parse()
+}
 
+func htmlParser(root, curr *string) []string {
+
+	result := make([]string, 50)
 	//make reguest
-	resp, _ := http.Get(*adr)
+	resp, _ := http.Get(*curr)
 
 	//return new html tokenizer for the given Reader
 	tz := html.NewTokenizer(resp.Body)
@@ -25,21 +36,30 @@ func main() {
 		switch token {
 		case html.ErrorToken:
 			// End of the document
-			return
+			return result
 		case html.StartTagToken, html.SelfClosingTagToken:
 			tag, hattr := tz.TagName()
 			isAnchor := string(tag)
 
 			if isAnchor == "a" && hattr {
 				_, v, _ := tz.TagAttr()
-				res := string(v)
-				if strings.HasPrefix(res, "http://") {
-					fmt.Println(res)
-				} else {
-					fmt.Println(*adr + res)
-				}
+				result = append(result,
+					linkMaker(root, curr, string(v)))
 			}
 		}
 	}
 	resp.Body.Close()
+
+	return result
+}
+
+func main() {
+
+	var adr = flag.String("flag", "http://xmpp.org", "http address")
+	flag.Parse()
+
+	larr := htmlParser(adr, adr)
+	for _, val := range larr {
+		fmt.Println(val)
+	}
 }
