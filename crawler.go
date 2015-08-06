7b3acfce05baefbae1995826971714229c7bae21
Katarina Sheremet <kate.romanowskaya@gmail.com>
2015-08-06 21:45:42 +0200

    Add depth of searching

--- crawler.go
+++ crawler.go
@@ -3,37 +3,44 @@ package main
 import (
 	"flag"
 	"fmt"
+	"github.com/deckarep/golang-set"
 	"golang.org/x/net/html"
 	"log"
 	"net/http"
 	"strings"
 )
 
-func linkMaker(root, curr *string, l string) string {
+//Links that checked or started to check
+var chLinks mapset.Set
+var depth *int
+var result mapset.Set
 
-	log.Println("original link", l)
+func linkMaker(root, curr string, l string) string {
+
+	log.Println("Original link", l)
 
 	if strings.HasPrefix(l, "http://") ||
 		(strings.HasPrefix(l, "https://")) {
-		log.Println("fixed link", l)
+		log.Println("Fixed link", l)
 		return l
 	}
 	if strings.HasPrefix(l, "/") {
-		log.Println("fixed link", *root+l)
-		return (*root + l)
+		log.Println("Fixed link", root+l)
+		return (root + l)
 	} else {
-		log.Println("fixed link", *curr+"/"+l)
-		return (*curr + "/" + l)
+		log.Println("Fixed link", curr+"/"+l)
+		return (curr + "/" + l)
 	}
 
 }
 
-func htmlParser(root, curr *string) []string {
+func htmlParser(root, curr string, cdepth int) {
 
-	result := make([]string, 0, 50)
+	log.Println("Start parsing", curr)
 
 	//make reguest
-	resp, _ := http.Get(*curr)
+	resp, _ := http.Get(curr)
+	defer resp.Body.Close()
 
 	//return new html tokenizer for the given Reader
 	tz := html.NewTokenizer(resp.Body)
@@ -44,10 +51,9 @@ func htmlParser(root, curr *string) []string {
 
 		// End of the document or error
 		case html.ErrorToken:
-			resp.Body.Close()
 			log.Println("End of document or error on the page ",
-				*curr)
-			return result
+				curr)
+			return
 		case html.StartTagToken, html.SelfClosingTagToken:
 			tag, _ := tz.TagName()
 			isAnchor := string(tag)
@@ -60,8 +66,17 @@ func htmlParser(root, curr *string) []string {
 				for mattr && string(k) != "href" {
 					k, v, mattr = tz.TagAttr()
 				}
-				result = append(result,
-					linkMaker(root, curr, string(v)))
+				if string(k) == "href" {
+					newlink := linkMaker(root, curr, string(v))
+					result.Add(newlink)
+					if cdepth < *depth && chLinks.Add(newlink) {
+						if strings.HasPrefix(newlink, root) {
+							htmlParser(root, newlink, cdepth+1)
+						} else {
+							htmlParser(curr, newlink, cdepth+1)
+						}
+					}
+				}
 			}
 		}
 	}
@@ -69,18 +84,24 @@ func htmlParser(root, curr *string) []string {
 }
 
 func main() {
+	chLinks = mapset.NewSet()
+	result = mapset.NewSet()
 
 	// Init values for the standart logger
 	// Lshortfile - file name and file number
 	log.SetFlags(log.Lshortfile)
 	var adr = flag.String("url", "http://xmpp.org", "http address")
+	depth = flag.Int("depth", 5, "depth of searching")
 	flag.Parse()
 	log.Println("get properties from command line")
+	log.Println(*depth)
 
-	larr := htmlParser(adr, adr)
+	chLinks.Add(*adr)
+	htmlParser(*adr, *adr, 0)
 
 	log.Println("print links")
-	for _, val := range larr {
+	for val := range result.Iter() {
 		fmt.Println(val)
 	}
+
 }
