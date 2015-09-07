762ba7c4eb31df020613d8621cdcfcae5ce859e6
Katarina Sheremet <kate.romanowskaya@gmail.com>
2015-09-07 11:27:01 +0200

    Add test for htmlParser

--- crawler_test.go
+++ crawler_test.go
@@ -1,7 +1,13 @@
 package main
 
 import (
+	"fmt"
+	"github.com/deckarep/golang-set"
+	"log"
+	"net/http"
+	"net/http/httptest"
 	"net/url"
+	"sync"
 	"testing"
 )
 
@@ -16,13 +22,74 @@ var tests = []testpairlink{
 	{"http://xmpp.org/value/", "#value2", "http://xmpp.org/value/"},
 }
 
+var page = `<HTML>
+<HEAD>
+<TITLE>Your Title Here</TITLE>
+</HEAD>
+<HR>
+<a href="http://somegreatsite.com">Link Name</a>
+is a link to another nifty site
+<H1>This is a Header</H1>
+<H2>This is a Medium Header</H2>
+Send me mail at <a href="mailto:support@yourcompany.com">
+support@yourcompany.com</a>.
+<P> This is a new paragraph!
+<P> <B>This is a new paragraph!</B>
+<a href="http://xmpp.org/about-xmpp/">About</a>
+<a href="http://xmpp.org/about-xmpp/faq/">FAQ</a>
+<BR> <B><I>This is a new sentence without a paragraph break, in bold italics.</I></B>
+<a href="http://wordpress.org/" rel="generator">WordPress</a>
+<HR>
+</BODY>
+</HTML>`
+
+var resultlinks = []string{"http://somegreatsite.com", "mailto:support@yourcompany.com",
+	"http://xmpp.org/about-xmpp/", "http://xmpp.org/about-xmpp/faq/",
+	"http://wordpress.org/"}
+
 func TestLinkMaker(t *testing.T) {
 	for _, pair := range tests {
 		ur, _ := url.Parse(pair.url)
 		lmres, _ := linkMaker(ur, pair.u)
 		res, _ := url.Parse(pair.res)
 		if *lmres != *res {
-			t.Error("For", pair.url, pair.u, "expected", pair.res, "got", lmres.String())
+			t.Error("For", pair.url, pair.u, "expected", pair.res,
+				"got", lmres.String())
+		}
+	}
+}
+
+func TestHtmlParser(t *testing.T) {
+	ts := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintln(w, page)
+	}))
+	defer ts.Close()
+
+	//Init values
+	var wq sync.WaitGroup
+	depth := new(int)
+	search := new(bool)
+	parallel := new(bool)
+	wq.Add(1)
+	*depth = 0
+	*search = true
+	*parallel = false
+
+	c := Crawler{mapset.NewSet(), mapset.NewSet(), depth, search, parallel,
+		wq}
+	c.chLinks.Add(ts.URL)
+	u, err := url.Parse(ts.URL)
+	if err != nil {
+		log.Println(err)
+	} else {
+		c.htmlParser(u, 0)
+		c.wq.Wait()
+		answ := mapset.NewSet()
+		for _, v := range resultlinks {
+			answ.Add(v)
+		}
+		if !answ.Equal(c.result) {
+			t.Error("Expected links and got are not the same")
 		}
 	}
 }
